2e124b0a1d76ffc2d92ade6b1cb807a2465cad0d
geonwee <gunni99@naver.com>
2025-01-17 14:49:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
+import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
 
-# 간단한 그래프 그리기
-st.title("Streamlit Matplotlib Test")
-
-# 그래프 생성
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3, 4], [10, 20, 25, 30], label="Example Line")
-ax.set_title("Example Matplotlib Plot")
-ax.legend()
-
-# Streamlit에서 그래프 렌더링
-st.pyplot(fig)
+# 데이터 로드
+df = pd.read_csv("teams_train.csv")  # 파일 경로 확인
+st.write("데이터프레임", df.head())  # 데이터 일부 출력
